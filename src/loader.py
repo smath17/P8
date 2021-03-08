97b77bc384d642d542d2739692224def6e40c73f
Simon Mathiasen <smath17@student.aau.dk>
2021-03-08 12:55:35 +0100

    Load, label and visualize
    Loaded data is tf.data.dataset format

--- src/loader.py
+++ src/loader.py
@@ -7,33 +7,70 @@ import tensorflow as tf
 # tensorflow-datasets lib
 import tensorflow_datasets as tfds
 import pathlib
+import matplotlib.pyplot as plt
 
 
-def load_data():
-    dataset_path = "C:\\Users\\the_p\\Desktop\\images"
+def load_data(data_path):
+    """
+    Loads and labels images from a directory. Splits images into training and validation sets.
+
+    :param data_path: The path to dataset directory
+    :return: A tuple consisting of 2 tf.data.Datset objects.The training and validation datasets respectively.
+    """
     # Re-create path into object-oriented system
-    data_dir = pathlib.Path(dataset_path)
-    image_count = len(list(data_dir.glob('*.tif')))
-    print(image_count)
-
-    images = list(data_dir.glob('*.tif'))
-    first = PIL.Image.open(str(images[0]))
-
-    # Show picture, cannot show .tif
-    # PIL.Image.Image.show(first)
+    data_dir = pathlib.Path(data_path)
 
     batch_size = 32
-    img_height = 421
-    img_width = 424
+    img_height = 180
+    img_width = 180
 
-    # TODO: tif not supported, convert to jpeg, change directory structure
+    # Classes defined by directory structure
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
         validation_split=0.2,
         subset="training",
         seed=123,
+        shuffle=True,
+        label_mode='binary',
         image_size=(img_height, img_width),
         batch_size=batch_size)
 
+    validation_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        shuffle=True,
+        label_mode='binary',
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
-load_data()
+    return train_ds, validation_ds
+
+
+def visualize_data(dataset, img_count):
+    """
+    Visualizes part of dataset in plot
+
+    :param dataset: The dataset to be visualized
+    :param img_count: Number of images to show
+    """
+
+    class_names = dataset.class_names
+
+    plt.figure(figsize=(10, 10))
+    for images, labels in dataset:
+        for i in range(img_count):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            # For the current index, get the label value (0.0 or 1.0)
+            label_value = int(labels[i])
+            # Use label value to lookup proper class name (Original, Tampered)
+            plt.title(class_names[label_value])
+            plt.axis("off")
+
+    plt.show()
+
+
+train_set, valid_set = load_data("C:\\Users\\the_p\\Desktop\\d_set")
+visualize_data(train_set, 9)
