661fef4cbd1724e1589183860aea5d99049055d2
Rynke <37619846+Rynke@users.noreply.github.com>
2021-03-18 16:35:39 +0100

    Image downloader
    - image_downloader.py

--- src/image_downloader.py
+++ src/image_downloader.py
@@ -16,26 +16,26 @@ def download_images():
                     "median_playtime", "owners", "price"]
 
     df_media = pd.read_csv("steam_media_data.csv", names=column_names_media)
-    df_apps = pd.read_csv("steam.csv", names=column_names_apps)
+    df_apps = pd.read_csv("steam.csv", names=column_names_apps, quotechar='"', skipinitialspace=True)
 
     if not os.path.exists("genres"):
         os.mkdir("genres")
 
-    if not os.path.exists("test_downloads"):
-        os.mkdir("test_downloads")
+    if not os.path.exists("all_images"):
+        os.mkdir("all_images")
 
     # iterate over rows with iterrows()
     apps_prev_row = 0
 
     for index_media, row_media in df_media.head(df_media.size).iterrows():
         # access data using column names
-        if index_media > 4115: # Fejl i csv ved 4118
+        if index_media > 0: # Fejl i csv ved 4118
             if index_media % 1 == 0:
                 json_string = "[" + row_media['screenshots'][1:-2].replace("\'", "\"") + "}]"
                 screenshots = json.loads(json_string)
                 screenshot_link = screenshots[0]["path_thumbnail"]
 
-                if not os.path.exists("test_downloads/" + row_media["steam_appid"] + ".jpg"):
+                if not os.path.exists("all_images/" + row_media["steam_appid"] + ".jpg"):
 
                     found = False
                     for index_apps, row_apps in df_apps.tail(df_apps.size - apps_prev_row).iterrows():
@@ -44,7 +44,7 @@ def download_images():
                             found = True
 
                             response = requests.get(screenshot_link)
-                            file = open("test_downloads/" + row_media["steam_appid"] + ".jpg", "wb")
+                            file = open("all_images/" + row_media["steam_appid"] + ".jpg", "wb")
                             file.write(response.content)
                             file.close()
 
@@ -52,12 +52,13 @@ def download_images():
                             for genre in genres:
                                 if not os.path.exists("genres/" + genre):
                                     os.mkdir("genres/" + genre)
-                                copyfile("test_downloads/" + row_media["steam_appid"] + ".jpg", "genres/" + genre + "/" + row_media["steam_appid"] + ".jpg")
+                                copyfile("all_images/" + row_media["steam_appid"] + ".jpg", "genres/" + genre + "/" + row_media["steam_appid"] + ".jpg")
                             break
                         else:
                             apps_prev_row += 1
                     if not found:
                         print(row_media["steam_appid"], "not found")
+                        apps_prev_row = 0
 
 
 time_before = time.time()
