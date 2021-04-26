896ed3766968da5017d8498105c5d3c85444aa8a
noddy95 <tomaten1995@hotmail.com>
2021-04-26 16:13:11 +0200

    fixed binary_labelling.py

--- src/binary_labels/binary_labelling.py
+++ src/binary_labels/binary_labelling.py
@@ -2,6 +2,7 @@ tags = open("tags.txt", "r")
 labels = []
 for x in tags:
   labels.append(x.strip())
+print("All labels:")
 print(labels)
 
 stop = 0
@@ -14,86 +15,25 @@ for game in games:
   split = game.split("[")
   split = split[1].split("]")
   game_tags = split[0]
-  if stop == 1: break
+  if stop == 2: break
   stop += 1
+  print("This game has following tags:")
   print(game_tags)
   game_tags = game_tags.split(",")
-  print(labels[0], game_tags[0])
-
-  print(game_list[0])
-  print(game_list)
 
 
 # Replace matching labels with true at given index
-  print(labels[0], game_tags[0])
+  #print(labels[0], game_tags[0])
   for game_tag in game_tags:
     for label in labels:
-      print("is", game_tag, "=", label)
-      if game_tag != label:
-        print("no")
+      #print("is", game_tag, "=", label)
+      if game_tag.strip() != label:
+        #print("no")
         counter += 1
       else:
-        print("yes")
-        print("counter = ", counter)
+        #print("yes")
+        #print("counter = ", counter)
         game_list[stop].insert(counter,True)
-        counter = 0
         break
-print(game_list[0])
-
-'''
-  game_tags = game_tags.split(",")
-  print(labels[0], game_tags[0])
-  for game_tag in game_tags:
-    for label in labels:
-      print("is", game_tag, "=", label)
-      if game_tag != label:
-        print("no")
-        counter += 1
-      else:
-        print("yes")
-        print("counter = ", counter)
-        game_list[stop].append([True])
-        counter = 0
-        break
-print(game_list)
-'''
-
-    #game_list[game.split(",").append()]
-'''
-tags = open("tags.txt", "r")
-labels = []
-for x in tags:
-  labels.append(x.strip())
-print(labels)
-
-stop = 0
-counter = 0
-games = open("app_labels.txt", "r")
-game_list = []
-
-# Prepare list with labels
-for game in games:
-  split = game.split("[")
-  split = split[1].split("]")
-  game_tags = split[0]
-  if stop == 1: break
-  stop += 1
-  print(game_tags)
-
-# Replace matching labels with true at given index
-  game_tags = game_tags.split(",")
-  print(labels[0], game_tags[0])
-  for game_tag in game_tags:
-    for label in labels:
-      print("is", game_tag, "=", label)
-      if game_tag != label:
-        print("no")
-        counter += 1
-      else:
-        print("yes")
-        print("counter = ", counter)
-        game_list[stop].insert(True,counter)
-        counter = 0
-        break
-
-'''
+  print(game_list[0])
+  counter = 0
