123420692f1aa0e3226128f9192bb5b3a85a6c14
HTML_Earth <htmlearth@gmail.com>
2021-05-11 11:20:47 +0200

    added data_stats.py
    script that generates csv with tag correspondence
    (currently misses first and last row (1980s, e_sports), but this is not really a problem, since it is the same as the leftmost and rightmost columns transposed

---
+++ src/utility/data_stats.py
@@ -0,0 +1,84 @@
+import pandas as pd
+
+# setup columns based on all_tags.txt
+columns = ["appid"]
+with open("../../resources/all_tags.txt") as file:
+    for line in file:
+        columns.append(line[:-1])
+
+# read from steamspy_tag_data.csv using previous columns
+df_tags = pd.read_csv("../../resources/steamspy_tag_data.csv", usecols=columns)
+
+tag_amount = len(columns)-1
+game_amount = len(df_tags)
+
+max_games = 100
+
+relation_matrix = []
+for y in range(tag_amount):
+    inner_list = []
+    for x in range(tag_amount):
+        inner_list.append(0)
+    relation_matrix.append(inner_list)
+
+game = 0
+# iterate through csv as tuples
+for row in df_tags.head(df_tags.size).itertuples():
+    #if game > max_games:
+    #    break
+
+    if game % 1000 == 0:
+        print(game, "/", game_amount)
+
+    index_h = 0
+    index_v = 0
+
+    # print("APPID: {}".format(row[1]))
+
+    # iterate through columns (tags)
+    while index_h < tag_amount:
+        tag_value_h = row[index_h + 2]
+        has_tag_h = tag_value_h > 0
+
+        while index_v < tag_amount:
+            tag_value_v = row[index_v + 2]
+            has_tag_v = tag_value_v > 0
+
+            if has_tag_h and has_tag_v:
+                # print("{}, {}".format(columns[index_h+1], columns[index_v+1]))
+                # print("{} {}".format(columns[index_h+1], row[index_h+2]))
+                relation_matrix[index_h][index_v] += 1
+
+            index_v += 1
+
+        index_v = 0
+        index_h += 1
+    game += 1
+
+
+file = open("../../resources/tag_stats.csv", "w")
+
+file.write("-,")
+col = 0
+while col < tag_amount-1:
+    file.write(columns[col+1])
+    if col < tag_amount-2:
+        file.write(",")
+    col += 1
+file.write("\n")
+
+row = 0
+while row < tag_amount-1:
+    # print(columns[row+1].rjust(25), relation_matrix[row])
+    row += 1
+    file.write(columns[row])
+    file.write(",")
+    col = 0
+    while col < tag_amount-1:
+        file.write(str(relation_matrix[row][col]))
+        if col < tag_amount-2:
+            file.write(",")
+        col += 1
+    file.write("\n")
+
+file.close()
